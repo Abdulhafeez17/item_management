9cc106a2faf9773b947bde6c334f7419723b884d
abdul hafeez <hafeez@Abduls-MacBook-Air.local>
2026-05-19 11:51:24 +0530

    add backend docker containerization

--- front-end/src/api/itemsApi.js
+++ front-end/src/api/itemsApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://item-management-ajm7.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
